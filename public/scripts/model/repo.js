57d993e50c541225e22c3889ac498e66257f5bfc
sharmarke <smarky292@gmail.com>
2017-06-28 23:03:31 -0700

    fixed some stuff

--- public/scripts/model/repo.js
+++ public/scripts/model/repo.js
@@ -8,22 +8,26 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/sharmarkei/repos',
-      method: 'GET',
-      headers: {
-        Authorization: `token ${githubToken}`
-      }
-    })
-      .then (data => {
-
-        data.forEach(repo => {
-          repos.all.push(repo);
-        })
-        console.log(repos.all);
-        callback();
-      })
+    $.get('/github/user/repos')
+    .then(data => repos.all = data, err => console.error(err))
+    .then(callback);
   };
+  //   $.ajax({
+  //     url: 'https://api.github.com/users/sharmarkei/repos',
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `token ${githubToken}`
+  //     }
+  //   })
+  //     .then (data => {
+  //
+  //       data.forEach(repo => {
+  //         repos.all.push(repo);
+  //       })
+  //       console.log(repos.all);
+  //       callback();
+  //     })
+  // };
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
   module.repos = repos;
