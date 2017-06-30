ca856c26d96e0093a73ce8abdfa9dc2d80043cc2
sharmarke <smarky292@gmail.com>
2017-06-30 11:10:53 -0700

    added request proxy require

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const requestProxy = require('express-request-proxy');
 
 app.use(express.static('./public'));
 
